e1180bf1eee50e44533a828d10c6702652d4d34a
Hastycodea <hastycodeandreh@gmail.com>
2024-07-22 08:15:05 +0300

    updated

--- src/main/java/org/example/easyquiz/QuizController.java
+++ src/main/java/org/example/easyquiz/QuizController.java
@@ -1,4 +1,15 @@
 package org.example.easyquiz;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
 public class QuizController {
+
+    @FXML
+    public Label question;
+
+    @FXML
+    public Button opt1, opt2, opt3, opt4;
+
 }
